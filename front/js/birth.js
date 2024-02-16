850a6b753b58b67aed9d9a8aedfe58cc3ba2bb5b
jun <junyealim@gmail.com>
2024-02-16 14:47:43 +0900

    .

--- front/js/birth.js
+++ front/js/birth.js
@@ -6,7 +6,7 @@ localStorage.setItem('char', randomNum);
 const cname = localStorage.getItem('name');
 let subment = document.querySelector(".subment");
 
-fetch('http://13.209.139.239:8000/mytest.html', {
+fetch('http://127.0.0.1:8000/mytest.html', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json'
